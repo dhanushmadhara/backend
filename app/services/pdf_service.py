7ed32e0e0276eb89168f629da89aa73ba38c6dac
dhanushmadhara <madharadhanush@gmail.com>
2025-12-07 12:59:33 +0530

    pdf_service.py

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,7 +1,10 @@
 from pdf2image import convert_from_bytes
+from typing import List
+from PIL import Image
+from ..config import settings
 
-def pdf_bytes_to_images(pdf_bytes: bytes, poppler_path=None):
-    kw = {}
-    if poppler_path:
-        kw["poppler_path"] = poppler_path
-    return convert_from_bytes(pdf_bytes, **kw)
+def pdf_bytes_to_images(pdf_bytes: bytes) -> List[Image.Image]:
+    kwargs = {}
+    if settings.poppler_path:
+        kwargs["poppler_path"] = settings.poppler_path
+    return convert_from_bytes(pdf_bytes, **kwargs)
